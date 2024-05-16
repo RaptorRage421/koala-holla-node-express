27604cbd7364c5525eb91364b12e0e3f0564c5e7
Tristan Bridges <mophead4@gmail.com>
2024-05-16 11:02:46 -0500

    db connection and get request created on router.js

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,17 +1,38 @@
 const express = require('express');
 const koalaRouter = express.Router();
+const pg = require('pg')
+
 
 // DB CONNECTION
-
+const pool = new pg.Pool({
+    host: 'localhost',
+    port: 5432,
+    database: 'koala_holla'
+})
 
 // GET
+koalaRouter.get('/', (req, res)=>{
+    let queryText = `SELECT * FROM "koalas" ORDER BY "name";`
 
+    pool.query(queryText)
+        .then((result)=>{
+            res.send(result.rows)
+        })
+        .catch((err)=>{
+            console.error('Error getting koalas', err)
+            res.sendStatus(500)
+        })
+})
 
 // POST
+koalaRouter.post('/', ()=>{
 
+})
 
 // PUT
+koalaRouter.post('/', ()=>{
 
+})
 
 // DELETE
 
