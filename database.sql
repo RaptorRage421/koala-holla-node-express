8ac57c99d71c424e38bc5728497e8dea06bd109d
Sumeiya Abdi <sumeiya619@gmail.com>
2024-05-16 11:50:34 -0500

    changed INSERT in database

--- database.sql
+++ database.sql
@@ -2,17 +2,18 @@ CREATE TABLE "koala" (
 	"id" SERIAL PRIMARY KEY,
 	"name" VARCHAR (250) NOT NULL,
 	"age" INTEGER,
-	"favorite color" VARCHAR (100) NOT NULL,
+	"favorite_color" VARCHAR (100) NOT NULL,
 	"ready_for_transfer" BOOLEAN DEFAULT FALSE,
 	"notes" VARCHAR (100) NOT NULL
 );
 
+
 INSERT INTO "koala"
-	("name", "age", "favorite color", "notes")
+	("name", "age", "favorite_color", "ready_for_transfer", "notes")
 VALUES 
-	('Scotty','4', 'Red', 'Born in Guatemala'),
-	('Jean',	 '5', 'Green', 'Allergic to lots of lava'),
-	('Ororo', '7', 'Yellow', 'Loves listening to Paula Abdul'),
-	('KLeaf',	'15', 'Purple', 'Never refuses a treat'),
-	('Charlie', '9', 'Orange',	'Favorite band is Nirvana'),
-	('Betsy', '4', 'Blue',	'Has a pet iguana')
+	('Scotty','4', 'Red', 'Y', 'Born in Guatemala'),
+	('Jean',	 '5', 'Green', 'Y', 'Allergic to lots of lava'),
+	('Ororo', '7', 'Yellow', 'N', 'Loves listening to Paula Abdul'),
+	('KLeaf',	'15', 'Purple', 'N', 'Never refuses a treat'),
+	('Charlie', '9', 'Orange',	'Y', 'Favorite band is Nirvana'),
+	('Betsy', '4', 'Blue',	'Y', 'Has a pet iguana')
